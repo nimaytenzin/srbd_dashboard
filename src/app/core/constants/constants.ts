9888ac4a2648289c9c21754e66b015afef077acc
kinley wangyel <kwangyel@gmail.com>
2024-02-06 15:34:08 +0600

    sdf

--- src/app/core/constants/constants.ts
+++ src/app/core/constants/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:4322';
-// export const API_URL = 'https://zhichar.bt/dev';
+// export const API_URL = 'http://localhost:4322';
+export const API_URL = 'https://zhichar.bt/dev';
 
 export const AUTHTOKENKEY = 'zhicharV2Auth';
